de293d9c31291cdc2a2c75eecca58bcd0a748806
Sushan Yadav <sushanyadav98@gmail.com>
2024-07-21 18:12:05 +0545

    feat: intersect

--- src/components/CounterTimeLines.tsx
+++ src/components/CounterTimeLines.tsx
@@ -1,7 +1,6 @@
 import { cn } from "@/functions/cn";
-import { useTouchDevice } from "@/hooks/useTouchDevice";
 import { memo, useEffect, useMemo, useRef, useState } from "react";
-import { useWindowSize } from "usehooks-ts";
+import { useMediaQuery, useWindowSize } from "usehooks-ts";
 import {
   motion,
   useAnimate,
@@ -18,7 +17,7 @@ export const CounterTimeLines = () => {
   const y = useMotionValue(0);
   const x = useMotionValue(0);
   const { setNumber } = useAnimatedCounter();
-  const { isTouchDevice } = useTouchDevice();
+  const isMobile = useMediaQuery("(max-width: 640px)");
 
   useMotionValueEvent(y, "change", (e) => {
     let newY = e % height;
@@ -47,8 +46,9 @@ export const CounterTimeLines = () => {
     >
       <Indicator />
       <motion.div
-        dragTransition={{ timeConstant: 100, power: 0.01 }}
-        drag={isTouchDevice ? "x" : "y"}
+        dragMomentum={false}
+        // dragTransition={{ timeConstant: 100, power: 0.01 }}
+        drag={isMobile ? "x" : "y"}
         whileDrag={{
           cursor: "grabbing",
         }}
@@ -68,7 +68,7 @@ export const CounterTimeLines = () => {
           animate(
             "#indicator",
             {
-              scale: isTouchDevice ? 1.3 : 1.5,
+              scale: isMobile ? 1.3 : 1.5,
             },
             {
               duration: 0.1,
@@ -77,32 +77,45 @@ export const CounterTimeLines = () => {
           );
         }}
         onDrag={(e: PointerEvent, info) => {
-          const velocityToCheck = isTouchDevice
-            ? info.velocity.x
-            : info.velocity.y;
-          const layerToCheck = isTouchDevice ? e.layerX : e.layerY;
+          const velocityToCheck = isMobile ? info.velocity.x : info.velocity.y;
+          const layerToCheck = isMobile ? info.offset.x : info.offset.y;
 
-          if (velocityToCheck === 0) return;
+          const ticks = document.querySelectorAll(".ticks");
+          const indicator = document.getElementById("indicator-inner");
 
-          if (velocityToCheck > 0) {
+          ticks.forEach((tick) => {
+            const t = tick.getBoundingClientRect();
+            const i = indicator?.getBoundingClientRect();
+
+            // check if tick is overlapping with indicator
             if (
-              Math.abs(layerToCheck) % 8 === 0 &&
-              prevLayer.current !== Math.abs(layerToCheck)
+              i?.top! < t.bottom! &&
+              i?.bottom! > t.top! &&
+              i?.left! < t.right! &&
+              i?.right! > t.left!
             ) {
-              setNumber((prev) => prev - 1);
-              prevLayer.current = Math.abs(layerToCheck);
+              const valToCheck = Math.ceil(layerToCheck);
+
+              if (velocityToCheck > 0) {
+                if (prevLayer.current !== valToCheck) {
+                  setNumber((prev) => prev - 1);
+                  prevLayer.current = valToCheck;
+                }
+              } else {
+                if (
+                  prevLayer.current !== valToCheck ||
+                  prevLayer.current + 1 === valToCheck
+                ) {
+                  setNumber((prev) => prev + 1);
+                  prevLayer.current = valToCheck;
+                }
+              }
+
+              return;
             }
-          } else {
-            if (
-              Math.abs(layerToCheck) % 8 === 0 &&
-              prevLayer.current !== Math.abs(layerToCheck)
-            ) {
-              setNumber((prev) => prev + 1);
-              prevLayer.current = Math.abs(layerToCheck);
-            }
-          }
+          });
         }}
-        style={isTouchDevice ? { x, touchAction: "none" } : { y }}
+        style={isMobile ? { x, touchAction: "none" } : { y }}
         className="w-[199vw] sm:w-auto sm:h-full cursor-grab sm:pl-6 flex sm:block"
       >
         <CounterTimeLine />
@@ -113,18 +126,29 @@ export const CounterTimeLines = () => {
 };
 
 const Indicator = () => {
-  const { isTouchDevice } = useTouchDevice();
+  const isMobile = useMediaQuery("(max-width: 640px)");
 
   return (
-    <motion.div
-      style={isTouchDevice ? { originY: "bottom" } : { originX: "left" }}
-      className={cn(
-        "bg-[#FFF833] z-40 absolute sm:ml-6 sm:top-1/2 sm:-translate-y-0.5 left-1/2 bottom-4 sm:bottom-auto sm:left-auto rounded-full pointer-events-none transition-all",
-        { "w-0.5 h-[44px]": isTouchDevice },
-        { "w-[44px] h-0.5": !isTouchDevice }
-      )}
-      id="indicator"
-    ></motion.div>
+    <>
+      <motion.div
+        style={isMobile ? { originY: "bottom" } : { originX: "left" }}
+        className={cn(
+          "bg-[#FFF833] z-40 absolute sm:ml-6 sm:top-1/2 sm:-translate-y-0.5 left-1/2 bottom-4 sm:bottom-auto sm:left-auto rounded-full pointer-events-none transition-all",
+          { "w-0.5 h-[44px]": isMobile },
+          { "w-[44px] h-0.5": !isMobile }
+        )}
+        id="indicator"
+      ></motion.div>
+
+      <div
+        className={cn(
+          "absolute sm:ml-6 sm:top-1/2 left-1/2 bottom-4 sm:bottom-auto sm:left-auto rounded-full pointer-events-none transition-all",
+          { "w-0.5 h-[44px]": isMobile },
+          { "w-[44px] h-px": !isMobile }
+        )}
+        id="indicator-inner"
+      ></div>
+    </>
   );
 };
 
@@ -132,20 +156,20 @@ export const CounterTimeLine = memo(() => {
   const { width = 0, height = 0 } = useWindowSize();
   const [numberOfLines, setNumberOfLines] = useState(0);
   const ref = useRef<HTMLDivElement>(null);
-  const { isTouchDevice } = useTouchDevice();
+  const isMobile = useMediaQuery("(max-width: 640px)");
 
   const GAP = useMemo(() => (width > 640 ? 22 : 12), [width]);
 
   useEffect(() => {
-    if (isTouchDevice === undefined) return;
-    const numberOfLines = !isTouchDevice
+    if (isMobile === undefined) return;
+    const numberOfLines = !isMobile
       ? Math.floor(height / GAP)
       : Math.floor(width / GAP);
 
     setNumberOfLines(numberOfLines);
-  }, [height, width, isTouchDevice, GAP]);
+  }, [height, width, isMobile, GAP]);
 
-  if (isTouchDevice === undefined) return null;
+  if (isMobile === undefined) return null;
 
   return (
     <div
@@ -155,8 +179,8 @@ export const CounterTimeLine = memo(() => {
     >
       <div
         className={cn("", {
-          "h-full w-px": !isTouchDevice,
-          "h-px w-full": isTouchDevice,
+          "h-full w-px": !isMobile,
+          "h-px w-full": isMobile,
         })}
         ref={ref}
       >
@@ -164,12 +188,12 @@ export const CounterTimeLine = memo(() => {
           <div
             key={i}
             className={cn(
-              "bg-[#113f36] ",
-              { "w-[10px] h-px absolute left-0": !isTouchDevice },
-              { "w-px h-[10px] absolute left-0": isTouchDevice }
+              "bg-[#113f36] ticks",
+              { "w-[10px] h-[0.5px] absolute left-0": !isMobile },
+              { "w-[0.5px] h-[10px] absolute left-0": isMobile }
             )}
             style={
-              !isTouchDevice
+              !isMobile
                 ? { top: `calc(${(i + 1) * GAP}px)` }
                 : { left: `calc(${(i + 1) * GAP}px)` }
             }
